8f47247bce817e8cf557ba5a493c8d82d23125a6
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-04-02 16:46:36 +0100

    corrigdo nome para adapter

--- src/projeto_armis/dependency_container.py
+++ src/projeto_armis/dependency_container.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
 
-from infrastructure.adapters.neo4j_connection import Neo4jConnection
+from infrastructure.adapters.neo4j_adapter import Neo4jAdapter
 from infrastructure.repositories.neo4j_repository import Neo4jRepository
 from application.services.files_service import FilesService
 from application.services.neo4j_service import Neo4jService
@@ -16,10 +16,8 @@ class DependencyContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
     wiring_config = containers.WiringConfiguration()
 
-    neo4j_connection = providers.Singleton(Neo4jConnection)
-    
-    neo4j_repository = providers.Factory(Neo4jRepository, connection = neo4j_connection)
-    #neo4j_repository = providers.Factory(Neo4jRepository)
+    neo4j_connection = providers.Singleton(Neo4jAdapter)
+    neo4j_repository = providers.Factory(Neo4jRepository, adapter = neo4j_connection)
     neo4j_service = providers.Factory(Neo4jService, repository=neo4j_repository)
     
     
--- src/projeto_armis/infrastructure/repositories/base_repository.py
+++ src/projeto_armis/infrastructure/repositories/base_repository.py
@@ -1,8 +1,8 @@
-from infrastructure.adapters.neo4j_connection import Neo4jConnection
+from infrastructure.adapters.neo4j_adapter import Neo4jAdapter
 
 class BaseRepository:
-    def __init__(self, connection : Neo4jConnection):
-        self.db = connection
+    def __init__(self, adapter : Neo4jAdapter):
+        self.db = adapter
         
     def run_query(self, query: str, params: dict = None):
         """Executa uma query Cypher no Neo4j"""
