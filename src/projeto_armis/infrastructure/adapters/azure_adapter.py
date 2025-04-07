014ddf107d8cd7426b7dadac09d80c3ad1efa508
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-04-07 16:42:17 +0100

    azure_adapter.py

--- src/projeto_armis/infrastructure/adapters/azure_adapter.py
+++ src/projeto_armis/infrastructure/adapters/azure_adapter.py
@@ -18,8 +18,18 @@ class AzureAdapter:
             openai_api_type=openAiApiType,
         ).with_structured_output(json_schema1)
         
+    def set_output_schema(self, json_schema):
+        self.llm = AzureChatOpenAI(
+            openai_api_version=openaiApiVersion,
+            #deployment_name=modeloGpt4o,
+            deployment_name=modeloGpt4omini,
+            azure_endpoint=getenv("ModelsEndpoint"),
+            openai_api_key=getenv("ModelsKey"),
+            openai_api_type=openAiApiType,
+        ).with_structured_output(json_schema)
+        
 
-    def call_llm(self, prompt_template: str, **kwargs):
+    def call_llm(self,prompt_template: str, **kwargs):
         prompt_template = PromptTemplate.from_template(prompt_template)
         prompt = prompt_template.format(**kwargs)
         message = HumanMessage(content=prompt)
