0a9d666ae40adb6805d334e833b2124da915b672
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-04-09 15:18:39 +0100

    Added endpoint to make questions via url.

--- src/projeto_armis/backend/presentation/routes.py
+++ src/projeto_armis/backend/presentation/routes.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from presentation.controllers.azure_controller import azure_blueprint
+from presentation.controllers.chat_controller import chat_blueprint
 from presentation.controllers.default_controller import default_blueprint
 from presentation.controllers.file_controller import files_blueprint
 from presentation.controllers.neo4j_controller import neo4j_blueprint
@@ -10,3 +11,4 @@ def setup_blueprints(app: Flask):
     app.register_blueprint(files_blueprint)
     app.register_blueprint(neo4j_blueprint)
     app.register_blueprint(azure_blueprint)
+    app.register_blueprint(chat_blueprint)
