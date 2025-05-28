c27808e6398b738a31231d0c80fa7569493b3275
Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>
2025-05-28 17:45:20 +0100

    Added FileController
    Signed-off-by: Eduardo Carreiro <109857329+Educ00@users.noreply.github.com>

--- src/projeto_armis/backend/presentation/controllers/file_controller.py
+++ src/projeto_armis/backend/presentation/controllers/file_controller.py
@@ -7,17 +7,18 @@ from dependency_container import DependencyContainer
 
 files_blueprint = Blueprint("Files", __name__, url_prefix="/files")
 
+class FileController:
 
-@files_blueprint.route("/import-file", methods=["POST"])
-@inject
-def upload_file(file_service : FilesService = Provide[DependencyContainer.files_service]):
-    if 'file' not in request.files:
-        return jsonify({"error": "Nenhum arquivo enviado"}), 400
-
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "Nome de arquivo inválido"}), 400
-
-    result = file_service.upload_file(file)
-
-    return jsonify(result), 201
+    @files_blueprint.route("/import-file", methods=["POST"])
+    @inject
+    def upload_file(self, file_service : FilesService = Provide[DependencyContainer.files_service]):
+        if 'file' not in request.files:
+            return jsonify({"error": "Nenhum arquivo enviado"}), 400
+    
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({"error": "Nome de arquivo inválido"}), 400
+    
+        result = file_service.upload_file(file)
+    
+        return jsonify(result), 201
